2bd15fe152f84452cbccd6e6785d3d1076801fa7
宇cccc <feyucccc@gmail.com>
2024-10-11 14:27:06 +0800

    fix：加人工

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Label } from "@/components/ui/label"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -20,71 +21,72 @@ type Init = {
     cb: string;
 }[]
 
-const 申通快递 = 1.8
-const daizi = 0.1
-
-
-const rengong = 0.5
-const 短袖 = 4.6 + rengong
-const 短裤 = 4.9 + rengong
-const 背心1 = 4.4 + rengong
-const 薄卫衣1 = 6.9 + rengong
-const 薄卫衣2 = 薄卫衣1 * 2
-const 长T恤 = 5.9 + rengong
-const 长假两件 = 6.4 + rengong
-const 短假两件 = 9.5 + rengong
-const 薄长裤 = 6.7 + rengong
-const 薄长裤2件 = 薄长裤 * 2
-
-// 加绒
-const 圆领卫衣加绒 = 9
-const 加绒裤子 = 9.2
-const 羊羔绒连帽 = 11
-const 羊羔绒裤 = 10.2
-const 羊羔绒圆领 = 9.5
-
-
-const styles = [
-    { label: '背心+短裤', value: 背心1 + 短裤, },
-    { label: '短袖+短裤', value: 短袖 + 短裤, },
-    { label: '短袖+薄长裤', value: 短裤 + 薄长裤, },
-    { label: '薄卫衣+薄长裤', value: 薄卫衣1 + 薄长裤, num: 2 },
-
-    { label: '短袖1件', value: 短袖, },
-    { label: '短袖2件', value: 短袖 * 2, },
-    { label: '短袖3件', value: 短袖 * 3 },
-
-    { label: '背心1件', value: 背心1, },
-    { label: '背心2件', value: 背心1 * 2, },
-    { label: '背心3件', value: 背心1 * 3, },
-    { label: '薄卫衣1件', value: 薄卫衣1 },
-    { label: '薄卫衣2件', value: 薄卫衣2 },
-    { label: '短裤1件', value: 短裤 },
-    { label: '短裤2件', value: 短裤 * 2 },
-
-
-    { label: '长T恤1件', value: 长T恤, },
-    { label: '长T恤2件', value: 长T恤 * 2, },
-
-    { label: '薄长裤1件', value: 薄长裤, },
-    { label: '薄长裤2件', value: 薄长裤2件, key: 4 },
-
-    { label: '长假两件1件', value: 长假两件, },
-    { label: '长假两件2件', value: 长假两件 * 2, },
-    { label: '短假两件', value: 短假两件, },
-    { label: '圆领卫衣加绒', value: 圆领卫衣加绒, },
-    { label: '加绒裤子', value: 加绒裤子, },
-    { label: '羊羔绒连帽', value: 羊羔绒连帽, },
-    { label: '羊羔绒裤', value: 羊羔绒裤, },
-    { label: '羊羔绒圆领', value: 羊羔绒圆领, },
-
-
-]
 
 
 export default function Order() {
 
-    const [dynamicVaildateForm, setDynamicVaildateForm] = useState([{}])
+    const [快递, setST] = useState(1.8)
+    const [rengong, setRG] = useState(0.7)
+
+    const daizi = 0.1
+
+    const 短袖 = 4.6 + rengong
+    const 短裤 = 4.9 + rengong
+    const 背心1 = 4.4 + rengong
+    const 薄卫衣1 = 6.9 + rengong
+    const 薄卫衣2 = 薄卫衣1 * 2
+    const 长T恤 = 5.9 + rengong
+    const 长假两件 = 6.4 + rengong
+    const 短假两件 = 9.5 + rengong
+    const 薄长裤 = 6.7 + rengong
+    const 薄长裤2件 = 薄长裤 * 2
+
+    // 加绒
+    const 圆领卫衣加绒 = 9 + rengong
+    const 加绒裤子 = 9.2 + rengong
+    const 羊羔绒连帽 = 11 + rengong
+    const 羊羔绒裤 = 10.2 + rengong
+    const 羊羔绒圆领 = 9.5 + rengong
+
+
+    const styles = [
+        { label: '背心+短裤', value: 背心1 + 短裤, },
+        { label: '短袖+短裤', value: 短袖 + 短裤, },
+        { label: '短袖+薄长裤', value: 短裤 + 薄长裤, },
+        { label: '薄卫衣+薄长裤', value: 薄卫衣1 + 薄长裤, num: 2 },
+
+        { label: '短袖1件', value: 短袖, },
+        { label: '短袖2件', value: 短袖 * 2, key: 99 },
+        { label: '短袖3件', value: 短袖 * 3 },
+
+        { label: '背心1件', value: 背心1, },
+        { label: '背心2件', value: 背心1 * 2, },
+        { label: '背心3件', value: 背心1 * 3, },
+        { label: '薄卫衣1件', value: 薄卫衣1 },
+        { label: '薄卫衣2件', value: 薄卫衣2 },
+        { label: '短裤1件', value: 短裤 },
+        { label: '短裤2件', value: 短裤 * 2 },
+
+
+        { label: '长T恤1件', value: 长T恤, },
+        { label: '长T恤2件', value: 长T恤 * 2, },
+
+        { label: '薄长裤1件', value: 薄长裤, },
+        { label: '薄长裤2件', value: 薄长裤2件, key: 4 },
+
+        { label: '长假两件1件', value: 长假两件, },
+        { label: '长假两件2件', value: 长假两件 * 2, },
+        { label: '短假两件', value: 短假两件, },
+        { label: '圆领卫衣加绒', value: 圆领卫衣加绒, },
+        { label: '加绒裤子', value: 加绒裤子, },
+        { label: '羊羔绒连帽', value: 羊羔绒连帽, },
+        { label: '羊羔绒裤', value: 羊羔绒裤, },
+        { label: '羊羔绒圆领', value: 羊羔绒圆领, },
+
+
+    ]
+
+    const [dynamicVaildateForm, setDynamicVaildateForm] = useState([{ cb: '', orderNumber: 1, numberOfPieces: 1, daizi: 0.1 }])
 
     useEffect(() => {
         const d = localStorage.getItem('dynamicVaildateForm')
@@ -166,6 +168,35 @@ export default function Order() {
 
 
     return <div className="p-10">
+
+        <div>
+            <div className="flex items-center space-x-2">
+                <Label htmlFor="kuaidi">快递费用（单）：</Label>
+                <Input
+                    className="w-auto"
+                    id="kuaidi"
+                    type="number"
+                    onChange={(e) => setST(+e.target.value)}
+                    value={快递}
+                    placeholder="快递费用"
+                />
+            </div>
+        </div>
+
+        <div className="mt-2">
+            <div className="flex items-center space-x-2">
+                <Label htmlFor="rengong">人工费用（件）：</Label>
+                <Input
+                    className="w-auto"
+                    id="rengong"
+                    type="number"
+                    onChange={(e) => setRG(+e.target.value)}
+                    value={rengong}
+                    placeholder="人工费用"
+                />
+            </div>
+        </div>
+
         <Button onClick={clearOrder} className="w-full my-6 bg-orange-400">清空</Button>
 
         {dynamicVaildateForm.map((item, index) => {
@@ -200,7 +231,7 @@ export default function Order() {
                     id="numberOfPieces"
                     placeholder="件数"
                 />
-                <span className="flex-auto">袋子：{item.daizi} + 单套（人工+货本）：{(+item.cb).toFixed(1)} (个本： {(+item.cb) + daizi  + 申通快递}) = {(item.numberOfPieces * +item.cb + item.daizi).toFixed(1)} </span>
+                <span className="flex-auto">袋子：{item.daizi} + 单套（人工+货本）：{(+item.cb).toFixed(1)} (个本： {(+item.cb) + daizi + 快递}) = {(item.numberOfPieces * +item.cb + item.daizi).toFixed(1)} </span>
                 <Button onClick={() => deleteOrder(index)}>删除</Button>
             </div>
         })
